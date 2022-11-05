3bb2d9c6a644589928fd91492c4bb786d58711cd
Don Yang <adong23664@gmail.com>
2022-11-05 16:04:16 +0800

    初始化 Spring Boot project

---
+++ src/test/java/com/don/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.don.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
